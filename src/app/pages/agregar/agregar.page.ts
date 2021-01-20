1bb28373717b488fc55f164d229b7fd17440c8dd
famonzon <fabianalexis06@gmail.com>
2021-01-20 14:32:49 -0300

    Wish List with Ionic

---
+++ src/app/pages/agregar/agregar.page.ts
@@ -0,0 +1,66 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { DeseosService } from '../../services/deseos.service';
+import { Lista } from '../../models/lista.model';
+import { ListaItem } from '../../models/lista-item.models';
+
+@Component({
+  selector: 'app-agregar',
+  templateUrl: './agregar.page.html',
+  styleUrls: ['./agregar.page.scss'],
+})
+export class AgregarPage implements OnInit {
+
+  lista: Lista;
+  nombreItem = '';
+
+  constructor( private deseosService: DeseosService,
+               private router: ActivatedRoute ) {
+
+    const listaId = this.router.snapshot.paramMap.get('listaId');
+    this.lista = this.deseosService.obtenerLista( listaId );
+
+   }
+
+  ngOnInit() {
+  }
+
+  agregarItem() {
+
+    if ( this.nombreItem.length === 0 ) {
+      return;
+    }
+
+    const nuevoItem = new ListaItem( this.nombreItem );
+    this.lista.items.push( nuevoItem );
+
+    this.nombreItem = '';
+    this.deseosService.guardarStorage();
+
+  }
+
+  cambioCheck( item: ListaItem ) {
+
+    const pendientes = this.lista.items
+                          .filter( itemData => !itemData.completado )
+                          .length;
+    if ( pendientes === 0) {
+      this.lista.terminadaEn = new Date();
+      this.lista.terminada = true;
+    } else {
+      this.lista.terminadaEn = null;
+      this.lista.terminada = false;
+    }
+
+    this.deseosService.guardarStorage();
+
+  }
+
+  eliminarItem( indice: number ) {
+
+    this.lista.items.splice( indice, 1 );
+
+    this.deseosService.guardarStorage();
+  }
+
+}
